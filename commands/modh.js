81b766075dd2a45ce1fb9889e4faf5c8a8485219
Antony Fantapié <fantapie.antony@gmail.com>
2019-08-24 13:42:00 +0200

    Fix modh

--- commands/modh.js
+++ commands/modh.js
@@ -1,13 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-
-    let iUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if (!iUser) return message.channel.send("Je n'ai pas trouvé l'utilisateur");
-
-    let roles = iUser.roles.array();
-
-    let icon = iUser.user.avatarURL;
+    
     let infoEmbed = new Discord.RichEmbed()
     .setDescription(`Liste des commandes de modération`)
     .setColor("#0110F0")
