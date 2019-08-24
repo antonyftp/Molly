193ff113552bb11e836e6a149dbdd8ae22fa93de
Antony Fantapié <fantapie.antony@gmail.com>
2019-08-24 13:44:44 +0200

    Fix modh

--- commands/modh.js
+++ commands/modh.js
@@ -1,11 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    
+
     let infoEmbed = new Discord.RichEmbed()
     .setDescription(`Liste des commandes de modération`)
     .setColor("#0110F0")
-    .setThumbnail(icon)
     .addField("Kick", "!kick @user raison")
     .addField("Ban", "!ban @user raison")
     .addField("Clear", "!clear nbmessages");
