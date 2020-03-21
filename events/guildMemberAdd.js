179c0a4c71d51fffa15ec9f3914e3f3a28f28527
Antony Fantapié <fantapie.antony@gmail.com>
2020-03-21 23:26:38 +0100

    Add an error management when player leave the discord when his not registered in the database

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,7 +3,6 @@ const mollydb = require("../js/mollydb");
 module.exports = (client, member) => {
     mollydb.query(`SELECT COUNT(discordID) as total from sys.members where discordID = '${member.id}'`, function (err, result) {
         if (err) throw err;
-        console.log(result[0].total);
         if (result[0].total === 0) {
             mollydb.query(`INSERT INTO sys.members (discordTag, discordID) VALUES ('${member.displayName}', '${member.id}')`, function (err, result) {
                 if (err) throw err;
